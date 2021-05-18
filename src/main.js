8f25382c14327778c041a6148b07c4b59c4be996
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2021-05-18 10:15:11 +0200

    Js file added

---
+++ src/main.js
@@ -0,0 +1,15 @@
+
+/*
+Receive an error message when the `form` is submitted if:
+  - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*/
+
+
+
+
+  let inputInfo = document.querySelector(input.value);
+
+
+  if (FirstName === null) {
+      let firstName = document.querySelector
+  }
