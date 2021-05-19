b6ada3d78d8110fd5002b614b5ae748765cdf6cf
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2021-05-19 10:16:02 +0200

    Error messages implemented

--- src/main.js
+++ src/main.js
@@ -4,44 +4,59 @@
 Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
 The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*/
 
-let name = document.querySelector("#first-name");
+let firstName = document.querySelector("#first-name");
 let lastName = document.querySelector("#last-name");
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
+let iconName = document.querySelector("#icon-error-name");
+let iconLastName = document.querySelector("#icon-error-lastname");
+let iconEmail = document.querySelector("#icon-error-email");
+let iconPassword = document.querySelector("#icon-error-password");
 
 
 function signUp(event) {
     event.preventDefault();
     
 
-    if (name.value == "") {
+    if (firstName.value == "") {
         let errorNameMessage = document.querySelector("#name-error");
         errorNameMessage.innerHTML = "First Name cannot be empty";
+        iconName.style.visibility = "visible";
+    } else {
+        null;
 
+
+    } if (lastName.value == ""){
         let errorLastNameMessage = document.querySelector("#lastname-error");
         errorLastNameMessage.innerHTML = "Last Name cannot be empty";
+        iconLastName.style.visibility = "visible";
+    } else {
+        null;
+     
 
+    } if (email.value == "") {
         let errorEmailMessage = document.querySelector("#email-error");
         errorEmailMessage.innerHTML = "Looks like this is not an email";
+        iconEmail.style.visibility = "visible";
 
+    } else {
+        null;
+       
 
+    } if (password.value == ""){
         let errorPasswordMessage = document.querySelector("#password-error");
         errorPasswordMessage.innerHTML = "Password cannot be empty";
-
-        /*Icon error*/
-
-        let iconError = document.querySelector("#icon-error");
-        iconError.innerHTML = "img";
-    
-        
+        iconPassword.style.visibility = "visible";
       
     } else {
         return null;
     }
 
+}
+
    
 
-}
+
 
 let button = document.querySelector("#button");
 button.addEventListener("click", signUp);
