69a35f847928687fe7fe279a76dcacf1a6d8ea61
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2021-05-19 09:43:05 +0200

    JS added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,56 @@
 
 /*
-Receive an error message when the `form` is submitted if:
-  - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
-  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*/
+
+Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
+The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*/
+
+let name = document.querySelector("#first-name");
+let lastName = document.querySelector("#last-name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+
+
+function signUp(event) {
+    event.preventDefault();
+    
+
+    if (name.value == "") {
+        let errorNameMessage = document.querySelector("#name-error");
+        errorNameMessage.innerHTML = "First Name cannot be empty";
+
+        let errorLastNameMessage = document.querySelector("#lastname-error");
+        errorLastNameMessage.innerHTML = "Last Name cannot be empty";
+
+        let errorEmailMessage = document.querySelector("#email-error");
+        errorEmailMessage.innerHTML = "Looks like this is not an email";
+
+
+        let errorPasswordMessage = document.querySelector("#password-error");
+        errorPasswordMessage.innerHTML = "Password cannot be empty";
+
+        /*Icon error*/
+
+        let iconError = document.querySelector("#icon-error");
+        iconError.innerHTML = "img";
+    
+        
+      
+    } else {
+        return null;
+    }
+
+   
+
+}
+
+let button = document.querySelector("#button");
+button.addEventListener("click", signUp);
 
 
 
 
-  let inputInfo = document.querySelector(input.value);
 
 
-  if (FirstName === null) {
-      let firstName = document.querySelector
-  }
+  
+
+  
